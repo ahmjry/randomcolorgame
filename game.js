844fb812af6d22e413d605b2c016548d462814a1
Jarryullah Ahmad <j.ahmad@mail.utoronto.ca>
2017-05-01 01:59:17 -0400

    changed name to index html for testing

--- game.js
+++ game.js
@@ -1,12 +1,27 @@
 var colors = [
-"rgb(255,0,0)",
-"rgb(0,255,0)",
-"rgb(0,0,255)",
-"rgb(255,255,0)",
-"rgb(0,255,255)",
-"rgb(255,0,255)",]
+"rgb(255, 0, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 0, 255)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(255, 0, 255)",]
 
 var squares = document.querySelectorAll(".square");
-for (var i = 0; i <= squares.length; i++) {
+var chosenColor = colors[1];
+var display = document.getElementById("display");
+display.textContent = chosenColor;
+
+for (var i = 0; i < squares.length; i++) {
 	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		var clickedColor = this.style.background;
+		if(clickedColor === chosenColor) {
+			alert("Correct");
+		} else {
+			this.style.background= "#331E36";
+		}
+	});
 }
+
